d4cc58a9108646296a4f28a83667ee9420e64007
Taylor Downs <downs.taylor@gmail.com>
2020-08-29 13:57:16 -0400

    model for fetch

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,5 +1,4 @@
 /** @module Adaptor */
-import { req, rawRequest } from './Client';
 import { setAuth, setUrl } from './Utils';
 import 'regenerator-runtime/runtime.js';
 import {
@@ -7,8 +6,6 @@ import {
   expandReferences,
   composeNextState,
 } from 'language-common';
-import cheerio from 'cheerio';
-import cheerioTableparser from 'cheerio-tableparser';
 import fs from 'fs';
 import parse from 'csv-parse';
 import AdmZip from 'adm-zip';
@@ -38,81 +35,58 @@ export function execute(...operations) {
   };
 }
 
-/**
- * Make a GET request
- * @public
- * @example
- *  get("/myendpoint", {
- *      query: {foo: "bar", a: 1},
- *      headers: {"content-type": "application/json"},
- *      authentication: {username: "user", password: "pass"}
- *    },
- *    function(state) {
- *      return state;
- *    }
- *  )
- * @function
- * @param {string} path - Path to resource
- * @param {object} params - Query, Headers and Authentication parameters
- * @param {function} callback - (Optional) Callback function
- * @returns {Operation}
- */
-export function get(path, params, callback) {
+export function fetch(url, output) {
   return state => {
-    const url = setUrl(state.configuration, path);
-
-    const {
-      query,
-      headers,
-      authentication,
-      body,
-      formData,
-      options,
-      ...rest
-    } = expandReferences(params)(state);
-
-    const auth = setAuth(state.configuration, authentication);
-
-    return req('GET', { url, query, auth, headers, rest }).then(response => {
-      const nextState = composeNextState(state, response);
-      if (callback) return callback(nextState);
-      return nextState;
-    });
-  };
-}
-
-export function fetch(urlToFile) {
-  return state => {
+    /* Create an empty file where we can save data */
+    let file = fs.createWriteStream(output);
+    /* Using Promises so that we can use the ASYNC AWAIT syntax */
     return new Promise((resolve, reject) => {
-      request(
-        { url: urlToFile, method: 'GET', encoding: null },
-        (err, res, body) => {
-          console.log(body);
-          resolve(body);
-          //state.data.body = body;
-        }
-      );
-    }).then(data => {
-      return { ...state, response: { body: data } };
+      let stream = request({
+        /* Here you should specify the exact link to the file you are trying to download */
+        uri: url,
+        // headers: {
+        //   Accept:
+        //     'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        //   'Accept-Encoding': 'gzip, deflate, br',
+        //   'Accept-Language':
+        //     'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+        //   'Cache-Control': 'max-age=0',
+        //   Connection: 'keep-alive',
+        //   'Upgrade-Insecure-Requests': '1',
+        //   'User-Agent':
+        //     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+        // },
+        /* GZIP true for most of the websites now, disable it if you don't need it */
+        // gzip: true,
+      })
+        .pipe(file)
+        .on('finish', () => {
+          console.log(`The file is finished downloading.`);
+          resolve();
+        })
+        .on('error', error => {
+          reject(error);
+        });
+    }).catch(error => {
+      console.log(`Something happened: ${error}`);
     });
   };
-  // make a get and unzip the response
 }
 
-export function unzip(pathToSave) {
+export function unzip(input, output) {
   // something that unzips from a CSV and allows the output to be used for hte
   // input of `load(data, options)`
 
   return state => {
-    let { response } = state;
-    console.log(response.body);
-
-    const zip = new AdmZip(response.body);
-    const zipEntries = zip.getEntries();
-    console.log(zipEntries.length);
-    zip.extractAllTo(pathToSave, true);
-
-    return state;
+    return new Promise((resolve, reject) => {
+      const zip = new AdmZip(input);
+      const zipEntries = zip.getEntries();
+      console.log(zipEntries.length);
+      zip.extractAllTo(output, true);
+    }).then(() => {
+      console.log('extracted!');
+      return state;
+    });
   };
 }
 
